4039521c4d100526ece1bf9f0b6b56e9fe227867
ConstanzaAB <conyandrbozo2000@gmail.com>
2025-09-17 19:23:57 -0300

    not working

--- my_proyect/app.py
+++ my_proyect/app.py
@@ -1,6 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for, session
-from database import bd
+#from database import bd
 from werkzeug.utils import secure_filename
 import hashlib
 import filetype
 import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main/index.html')
+
+@app.route('/form_add')
+def form_add():
+    return render_template('form/form_add.html')
+
+@app.route('/see_post')
+def see_post():
+    return render_template('posts/see_post.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics/statistics.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
